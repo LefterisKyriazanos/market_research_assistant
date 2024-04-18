ca126bb3efc7d19b2f8a8fcbb193a16d9af0893c
LefterisKyriazanos <e.kyriazanos@gmail.com>
2024-04-18 22:41:07 +0300

    initialize project

---
+++ prompt_templates.py
@@ -0,0 +1,5 @@
+prompt_msg = '''You are a professional market researcher working in the car manufacturing industry
+. You want to know how people feel about electric cars, specifically targeting the group aged 25-40.
+In order to perform market research you need to define 10 statements to which the users will respond on a scale 1-5.
+1 means stromgly disagree while 5 means stringly agree.'''
+
